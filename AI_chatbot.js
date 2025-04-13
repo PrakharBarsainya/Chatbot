ef38cba0ba0fbd8bd5b74be3e3539f2924642286
PRAKHAR BARSAINYA <148670847+PrakharBarsainya@users.noreply.github.com>
2025-04-13 22:37:57 +0530

    Update AI_chatbot.js

--- AI_chatbot.js
+++ AI_chatbot.js
@@ -1,4 +1,4 @@
-const API_KEY = 'AIzaSyAEOdkgAuRPvFh7G1KMkpGcBDJSR3YVWLo';
+const API_KEY = 'API_KEY_ENTERY_WILL_BE_HERE';
 // Replace with your actual Gemini API key – this stores the API key to authenticate requests to the Gemini API.
 
 const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
